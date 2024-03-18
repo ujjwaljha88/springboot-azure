c797bba9fdf08338c065621b9edeac1442a62815
Ujjwal <r_ujjwal88@yahoo.co.in>
2024-03-18 14:26:49 +0530

    first commit

---
+++ src/test/java/com/pagonxt/springboot/azure/SpringbootAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pagonxt.springboot.azure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
